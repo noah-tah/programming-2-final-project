522c0f2789f920a57035da1accc059814a08cd46
Noah Tah <noahtah21@gmail.com>
2025-05-01 14:55:51 -0500

    feat: improved cli

--- final-project-noah-tah.cpp
+++ final-project-noah-tah.cpp
@@ -51,7 +51,7 @@ void printBinary(const std::string& message) {
 }
 
 void waitForInput() {
-    std::cout << "Press Enter to continue...";
+    std::cout << "\nPress Enter to continue...";
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
     // Clear the screen based on operating system
@@ -82,29 +82,57 @@ void encryptionDemonstration() {
     int SHIFT = 3;
     std::string KEY = "SWORD";
 
-    std::cout << "\nOriginal message: " << TEST_MESSAGE << std::endl;
+    std::cout << "\nOriginal message: " << "\"" << TEST_MESSAGE << "\"" << std::endl;
 
     std::cout << "\nEncrypting message..." << std::endl;
     
     std::string caesarEncryptedMessage = caesarShiftEncryption(TEST_MESSAGE, SHIFT);
-    std::cout << "\nCaesar encrypted: " << caesarEncryptedMessage << std::endl;
+    std::cout << "\nCaesar encrypted: " << "\"" << caesarEncryptedMessage << "\"" << std::endl;
 
     std::string finalXorEncryptedMessage = xorEncryption(caesarEncryptedMessage, KEY);
-    std::cout << "\nAfter XOR: " << finalXorEncryptedMessage << std::endl;
-    std::cout << "\nBinary representation of XOR encrypted message: \n\n";
+    std::cout << "\nAfter XOR: " << "\"" << finalXorEncryptedMessage << "\"" << std::endl;
+    waitForInput();
+    clearScreen();
+    std::cout << "The following screen prints the binary representation of the XOR encrypted message." << std::endl;
+    waitForInput();
+    clearScreen();
+    std::cout << "Binary representation of XOR encrypted message: \n\n";
     printBinary(finalXorEncryptedMessage);
+    waitForInput();
+    clearScreen();
+    std::cout << "Now we will begin the process of decrypting the message." << std::endl;
+    waitForInput();
+    clearScreen();
 
-    std::cout << "\nDecrypting message..." << std::endl;
+    std::cout << "Decrypting message..." << std::endl;
+    
+    std::cout << "\nMessage before decryption: " << "\"" << finalXorEncryptedMessage << "\"" << std::endl;
 
     std::string xorDecryptedMessage = xorDecryption(finalXorEncryptedMessage, KEY);
-    std::cout << "\nAfter XOR decryption: " << xorDecryptedMessage << std::endl;
+    std::cout << "\nAfter XOR decryption: " << "\"" <<  xorDecryptedMessage << "\"" << std::endl;
 
     std::string finalDecryptedMessage = caesarShiftDecryption(xorDecryptedMessage, SHIFT);
-    std::cout << "\nFinal decrypted: " << finalDecryptedMessage << std::endl;
+    std::cout << "\nFinal decrypted message: " << "\"" <<finalDecryptedMessage << "\"" << std::endl;
+    waitForInput();
+    clearScreen();
+}
+
+void programIntroduction() {
+    std::cout << "Welcome to the Encryption Demonstration Program!" << std::endl;
+    std::cout << "This program demonstrates a simple encryption and decryption process." << std::endl;
+    std::cout << "It uses Caesar Shift and XOR encryption methods." << std::endl;
+    waitForInput();
+    clearScreen();
+    std::cout << "The program will now demonstrate the encryption and decryption process." << std::endl;
+    std::cout << "\nYou will see the original message before any encryption occurs." << std::endl;
+    std::cout << "\nThen, the message will be encrypted using the Caesar Shift method." << std::endl;
+    std::cout << "\nNext, the encrypted message will be further encrypted using XOR encryption." << std::endl;
+    waitForInput();
     clearScreen();
 }
 
 int main() {
+    programIntroduction();
 
     encryptionDemonstration();
     
